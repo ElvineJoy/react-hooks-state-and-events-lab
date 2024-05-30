010a36a4e4d9e36c574bc9a4eaba341bd57e700a
ElvineJoy <elvinkuiyungest@gmail.com>
2024-05-30 09:59:48 +0300

    UseState understood

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+  console.log(inCart, setInCart);
+
+  function handleAddToCart () {
+    setInCart(!inCart);
+  }
+
+  const itemClass = inCart? "in-cart" : ""
   return (
-    <li className="">
+    <li className = {itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart}>{inCart ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
